8032098b1dad72069c30e7fb900857b829f422d7
MARS <leprcoin@gmail.com>
2023-02-02 23:35:38 +0400

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+int[,] CreateMatrix(int length, int width)
+{
+    int[,] matrix = new int[length, width];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+           matrix[i,j] = i + j;  
+        }        
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int ReadInt(string massage)
+{
+    Console.Write(massage);
+    return int.Parse(Console.ReadLine()!);
+} 
+
+int length = ReadInt("Enter amount of rows m: ");
+int width = ReadInt("Enter amount of columns n: ");
+//int left = ReadInt("Enter left side elements: ");
+//int right = ReadInt("Enter right side elements: ");
+
+int[,] matrix = CreateMatrix(length, width);
+PrintMatrix(matrix);
